c1a35842a502001b62767a010994c0554aee81cd
Prasun Saurabh <prasun.saurabh@uzh.ch>
2022-02-17 13:34:23 +0100

    Changing the random chat logic

--- Flask-Server/randomChat.py
+++ Flask-Server/randomChat.py
@@ -1,24 +1,25 @@
 from random import randrange
 
 init_max = 1
-chat_choice = [0,0,0,0]
+chat_choice = [0, 0, 0, 0]
 flag = 0
 last_choice = -1
 
-def check_last_choice_exception():
-    last_choice_available_flag = 0
-    all_other_exhausted_flag = 0
-    if chat_choice[last_choice] < init_max:
-        last_choice_available_flag = 1
-        for i in range(0,4):
-            if i != last_choice:
-                if chat_choice[i] == init_max:
-                    all_other_exhausted_flag += 1
 
-    if last_choice_available_flag == 1 and all_other_exhausted_flag == 3:
-        return True
-    else:
-        return False
+# def check_last_choice_exception():
+#     last_choice_available_flag = 0
+#     all_other_exhausted_flag = 0
+#     if chat_choice[last_choice] < init_max:
+#         last_choice_available_flag = 1
+#         for i in range(0,4):
+#             if i != last_choice:
+#                 if chat_choice[i] == init_max:
+#                     all_other_exhausted_flag += 1
+#
+#     if last_choice_available_flag == 1 and all_other_exhausted_flag == 3:
+#         return True
+#     else:
+#         return False
 
 def check_max():
     chat_set = set(chat_choice)
@@ -30,28 +31,16 @@ def check_max():
 
 
 def select_chat_room():
-    global flag,last_choice, init_max
+    global flag, last_choice, init_max
     final_choice = -1
     while True:
         choice = randrange(4)
         if chat_choice[choice] < init_max:
-            if check_last_choice_exception():
-                # print("Valid choice from last exception - " + str(choice))
-                chat_choice[choice] += 1
-                flag = 1
-                last_choice = choice
-                final_choice = choice
-                break
-            else:
-                if choice != last_choice:
-                    # print("Valid choice is - "+ str(choice))
-                    chat_choice[choice] += 1
-                    flag = 1
-                    last_choice = choice
-                    final_choice = choice
-                    break
-                else:
-                    continue
+            chat_choice[choice] += 1
+            flag = 1
+            last_choice = choice
+            final_choice = choice
+            break
         else:
             if check_max():
                 init_max = init_max + 1
@@ -60,7 +49,8 @@ def select_chat_room():
 
     return final_choice
 
-# for i in range(0,20):
-#     print(select_chat_room())
-#
-# print(chat_choice)
+
+for i in range(0,20):
+    print(select_chat_room())
+
+print(chat_choice)
